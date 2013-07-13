0ea281b2dd404e2db0187ba3b2c7b66a3a940812
Niall O'Higgins <niallo@frozenridge.co>
2013-07-13 15:40:45 -0600

    implement

---
+++ index.js
@@ -0,0 +1,44 @@
+// Create a function to implement PassportJS deserializeUser function.
+// Requires a 'level-userdb' instance as parameter.
+function mkDeserializeUser(dbi) {
+  return function(email, done) {
+    function find() {
+      dbi.findUser(email, found())
+    }
+    function found(err, user) {
+      done(err, user)
+    }
+    find()
+  }
+})
+
+// Create a function to implement PassportJS serializeUser function.
+function serializeUser(user, done) {
+  done(null, user.email)
+}
+
+// Create a function to implement PassportJS-local verify function.
+// Requires a 'level-userdb' instance as parameter.
+function mkLocalStrategyVerify(dbi) {
+  return function(email, password, done) {
+    dbi.checkPassword(email, password, function(err, user) {
+      if (err) {
+        console.log("auth fail for email %s", email)
+        return done(null, false, { message: err})
+      }
+      console.log("auth success for email %s", email)
+      return done(null, user)
+
+    })
+  }
+}
+
+
+module.exports = function(dbi) {
+
+  return {
+    deserializeUser: mkDeserializeUser(dbi),
+    serializeUser: serializeUser,
+    localStrategyVerify: mkLocalStrategyVerify(dbi),
+  }
+}
